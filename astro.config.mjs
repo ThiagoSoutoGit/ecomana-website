bd02c35d957633297d34e1812b337f97f4bfc303
thiagosouto <thiago@skinnyrobot.co.nz>
2024-10-07 12:21:56 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
-  }), compress()],
+  }), compress(), react()],
 })
